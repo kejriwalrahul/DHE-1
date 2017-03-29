92895517e9b104fbf9ec381984fa720236dfd008
Rahul Kejriwal <kejriwalrahul@outlook.com>
2017-03-29 20:35:42 +0530

    fix

--- Code/Production/encrypt.c
+++ Code/Production/encrypt.c
@@ -1,23 +1,34 @@
 #include <stdio.h>
 #include "sbox.c"
 
+const int NO_OF_SBOXES = 16;
+
 // 8x8 SBoxes
-extern char sbox_8_8[16][256];
-extern char inv_sbox_8_8[16][256];
+extern char sbox_8_8[NO_OF_SBOXES][256];
+extern char inv_sbox_8_8[NO_OF_SBOXES][256];
 
 // 6x4 SBoxes
-extern char sboxes_6_4[16][64];
+extern char sboxes_6_4[NO_OF_SBOXES][64];
 
 typedef struct {
-	char block[16];
+	char block[NO_OF_SBOXES];
 } StageBits;
 
 void FiestelRound(StageBits *s){
 	
 }
 
-void SPNRound(StageBits *s){
-	
+void SPNRound(StageBits *s, StageBits *key){
+	int i;
+
+	// Key Addition
+	for(i=0; i<NO_OF_SBOXES; i++){
+		
+	}
+
+	// Substitution
+
+	// Permutation
 }
 
 int main(int argc, char** argv){
