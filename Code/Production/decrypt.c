7ce9c0c02733a5edb60324aaff119b30c40bcc08
Bikash Gogoi <bikashgogoi001@gmail.com>
2017-03-30 15:20:10 +0530

    bug fixed at Fiestel Round

--- Code/Production/decrypt.c
+++ Code/Production/decrypt.c
@@ -46,11 +46,19 @@ void SPNRound(StageBits *s, StageBits *key){
 		s->block[i] ^= key->block[i]; 		
 }
 
-/*
-void FiestelRound(StageBits *s, KeyType *key){
-	unsigned char leftHalf[6], rightHalf[6];
-	unsigned char lch, rch, ltemp, rtemp, tempval;
+
+void FiestelRound(StageBits *s, char key[]){
+	char leftHalf[6], rightHalf[6];
+	char lch, rch, ltemp, rtemp, tempval;
 	int i, j;
+
+	unsigned char temp;
+	//swap left half with right half of cipher text
+	for (i=0; i<8; i++) {
+		temp = s->block[i];
+		s->block[i] = s->block[8+i];
+		s->block[8+i] = temp;
+	}
 
 	// expansion of inputs
 	for (i=0; i<6; ++i) {
@@ -60,8 +68,8 @@ void FiestelRound(StageBits *s, KeyType *key){
 			lch = lch  << 1;
 			rch = rch << 1;
 			tempval = expansion[i][j]-1;
-			ltemp = s.block[8+tempval/8] << (7 - tempval % 8);
-			rtemp = s.block[12+tempval/8] << (7 - tempval % 8)
+			ltemp = s->block[8+tempval/8] << (7 - tempval % 8);
+			rtemp = s->block[12+tempval/8] << (7 - tempval % 8);
 			lch = lch | (ltemp >> 7);
 			rch = rch | (rtemp >> 7);
 		}
@@ -72,7 +80,7 @@ void FiestelRound(StageBits *s, KeyType *key){
 	//key mixing
 	for (i=0; i<6; ++i) {
 		leftHalf[i] ^= key[i];
-		rightHalf[i] ^= key[i+6]
+		rightHalf[i] ^= key[i+6];
 	}
 
 	unsigned char out[8];
@@ -86,19 +94,15 @@ void FiestelRound(StageBits *s, KeyType *key){
 		left = (left << 8) | leftHalf[i];
 		right = (right << 8) | rightHalf[i];
 	}
-	j=42
+	j=42;
 	for (i=0; i<4; ++i) {
-		lpad = left << (58-j);
-		lpad = lpad >> (58-j);
-		rpad = right << (58-j);
-		rpad = rpad >> (58-j);
-		out[i] = (0x0f && (lpad >> j));
-		out[4+i] = (0x0f && (rpad >> j));
+		lpad = left & (0xffffffffffffffff >> (58-j));
+		rpad = rpad & (0xffffffffffffffff >> (58-j));
+		out[i] = (0x0f & (lpad >> j));
+		out[4+i] = (0x0f & (rpad >> j));
 		j = j-6;
-		lpad = left << (58-j);
-		lpad = lpad >> (58-j);
-		rpad = right << (58-j);
-		rpad = rpad >> (58-j);
+		lpad = left & (0xffffffffffffffff >> (58-j));
+		rpad = rpad & (0xffffffffffffffff >> (58-j));
 		out[i] = (out[i] << 4) | (lpad >> j);
 		out[4+i] = (out[4+i] << 4) | (rpad >> j);
 		j = j-6;
@@ -117,16 +121,18 @@ void FiestelRound(StageBits *s, KeyType *key){
 			lpad = lpad | (rpad << (fperm[i][j] - 1));
 		}
 	}
-	for (i=2; i<4; ++i) {
+	// printf("%lu\n",lpad);
+	for (i=0; i<2; ++i) {
 		for (j=0; j<8; j++) {
 			lch = out[4+ (i<<2) + j/8] << j;
 			lch = lch >> (7-j);
 			rpad = lpad;
-			rpad = rpad << (64 - fperm[i][j]);
-			rpad = lch | (rpad >> (fperm[i][j] - 1));
-			lpad = lpad | (rpad << (fperm[i][j] - 1));
+			rpad = rpad << (64 - fperm[2+i][j]);
+			rpad = lch | (rpad >> (fperm[2+i][j] - 1));
+			lpad = lpad | (rpad << (fperm[2+i][j] - 1));
 		}
 	}
+	// printf("%lu\n",lpad);
 	for (i=7; i>=0; --i) {
 		out[i] = lpad;
 		lpad = lpad >> 8;
@@ -134,26 +140,41 @@ void FiestelRound(StageBits *s, KeyType *key){
 
 	//xor lefthalf with fiestel output
 	for (i=0; i<8; ++i) {
-		out[i] ^= s.block[i];
+		out[i] ^= s->block[i];
+	}
+	for (i=0; i<8; ++i) {
+		s->block[i] = s->block[8+i];
+		s->block[8+i] = out[i];
 	}
 
-	for (i=0; i<8; ++i) {
-		s.block[i] = s.block[8+i];
-		s.block[8+i] = out[i];
+
+	for (i=0; i<8; i++) {
+		temp = s->block[i];
+		s->block[i] = s->block[8+i];
+		s->block[8+i] = temp;
 	}
 }
-*/
+
 
 int main(int argc, unsigned char** argv){
 	// Test Vector
 	StageBits s = { {0x55, 0xaa, 0x4, 0x55, 0x44, 0xaa, 0x0, 0xdd, 0x2a, 0x55, 0x0, } };
 	StageBits k = { {16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1} };
 
-	SPNRound(&s, &k);
+	char key[12] = {1,2,3,4,5,6,7,8,9,10,11,12};
+	StageBits plaintext = {{105,106,107,108,109,110,111,112,158,157,156,155,154,153,152,151}};
+	FiestelRound(&plaintext, key);
+	// // SPNRound(&s, &k);
 	int i;
 	for(i=0; i<16; i++)
-		printf("%x\t", (s.block[i]));
-
+		printf("%c,", plaintext.block[i]);
 	printf("\n");
+	
+	// SPNRound(&s, &k);
+	// int i;
+	// for(i=0; i<16; i++)
+	// 	printf("%x\t", (s.block[i]));
+
+	// printf("\n");
 	return 0;
 }
